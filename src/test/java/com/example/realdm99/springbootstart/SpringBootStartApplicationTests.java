59e99795c260c7abba932562a85c491e7c5d863f
Shin dong myung <realdm99@gmail.com>
2019-11-29 16:45:40 +0900

    add chapter 02

--- src/test/java/com/example/realdm99/springbootstart/SpringBootStartApplicationTests.java
+++ src/test/java/com/example/realdm99/springbootstart/SpringBootStartApplicationTests.java
@@ -1,13 +1,56 @@
 package com.example.realdm99.springbootstart;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.realdm99.springbootstart.entity.Board;
+import com.example.realdm99.springbootstart.repository.BoardRepository;
 
 @SpringBootTest
 class SpringBootStartApplicationTests {
+
+    @Autowired
+    BoardRepository boardRepository;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void testInsert() {
+        Board board = new Board();
+        board.setTitle("게시물의 제목");
+        board.setContent("게시물 내용 넣기....");
+        board.setWriter("user00");
+        boardRepository.save(board);
+    }
+
+    @Test
+    public void testRead() {
+        boardRepository.findById(1L).ifPresent((board) -> {
+            System.out.println(board);
+        });
+    }
+
+    @Test
+    public void testUpdate() {
+        System.out.println("Read First.................");
+        Board board = boardRepository.findById(1L).orElseThrow(RuntimeException::new);
+
+        System.out.println("Update Title ..............");
+        board.setTitle("수정된 제목");
+        System.out.println("Call Save()................");
+        boardRepository.save(board);
+    }
+
+    @Test
+    public void testDelete() {
+        System.out.println("DELETE Entity ");
+        Board board = new Board();
+        board.setBno(1L);
+        boardRepository.delete(board);
+    }
 }
