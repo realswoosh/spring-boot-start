3080edb65beac388f2604640786d0958c99a0d90
Shin dong myung <realdm99@gmail.com>
2020-01-07 14:30:47 +0900

    thymeleaf some sample

---
+++ chapter5/src/main/java/com/example/realdm99/chapter5/controller/SampleController.java
@@ -0,0 +1,41 @@
+package com.example.realdm99.chapter5.controller;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.realdm99.chapter5.domain.MemberVO;
+
+@Controller
+public class SampleController {
+    @GetMapping("/sample1")
+    public String sample1(Model model) {
+        //model.addAttribute("greeting", "hello world");
+        //model.addAttribute("greeting", "안녕하세요");
+        return "sample1_new";
+    }
+
+    @GetMapping("/sample2")
+    public void sample2(Model model) {
+        MemberVO vo = new MemberVO(123, "u00", "p00", "홍길동",
+                                   new Timestamp(System.currentTimeMillis()));
+        model.addAttribute("vo", vo);
+    }
+
+    @GetMapping("/sample3")
+    public void sample3(Model model) {
+        List<MemberVO> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(
+                    new MemberVO(123,
+                                 "u0" + i,
+                                 "po" + i,
+                                 "홍길동" + i, new Timestamp(System.currentTimeMillis())));
+        }
+        model.addAttribute("list", list);
+    }
+}
