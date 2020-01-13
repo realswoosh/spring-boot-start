c83402658e577399b7fdc97d343fb38c9a96f2f2
Shin dong myung <realdm99@gmail.com>
2020-01-13 15:29:06 +0900

    sample4, sample5

--- chapter5/src/main/java/com/example/realdm99/chapter5/controller/SampleController.java
+++ chapter5/src/main/java/com/example/realdm99/chapter5/controller/SampleController.java
@@ -38,4 +38,36 @@ public class SampleController {
         }
         model.addAttribute("list", list);
     }
+
+    @GetMapping("/sample4")
+    public void sample4(Model model) {
+        List<MemberVO> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new MemberVO(
+                    i, "u000" + i % 3,
+                    "p000" + i % 3,
+                    "홍길동" + i,
+                    new Timestamp(System.currentTimeMillis())));
+        }
+        model.addAttribute("list", list);
+    }
+
+    @GetMapping("/sample5")
+    public void sample5(Model model) {
+        String result = "SUCCESS";
+        model.addAttribute("result", result);
+    }
+
+    @GetMapping("/sample6")
+    public void sample6(Model model) {
+        List<MemberVO> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(new MemberVO(i, "u0" + i, "p0" + i, "홍길동" + i,
+                                  new Timestamp(System.currentTimeMillis())));
+        }
+        model.addAttribute("list", list);
+
+        String result = "SUCCESS";
+        model.addAttribute("result", result);
+    }
 }
