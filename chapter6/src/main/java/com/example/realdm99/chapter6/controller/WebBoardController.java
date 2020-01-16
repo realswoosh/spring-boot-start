71a487c61c5d3560827a38c2cb5a1bc9189dbb29
Shin dong myung <realdm99@gmail.com>
2020-01-16 16:51:32 +0900

    layout decorator fix

--- chapter6/src/main/java/com/example/realdm99/chapter6/controller/WebBoardController.java
+++ chapter6/src/main/java/com/example/realdm99/chapter6/controller/WebBoardController.java
@@ -1,4 +1,18 @@
 package com.example.realdm99.chapter6.controller;
 
-public class WebBoardContoller {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.java.Log;
+
+@Controller
+@RequestMapping("/boards/")
+@Log
+public class WebBoardController {
+
+    @GetMapping("/list")
+    public void list() {
+        log.info("list() called...");
+    }
 }
