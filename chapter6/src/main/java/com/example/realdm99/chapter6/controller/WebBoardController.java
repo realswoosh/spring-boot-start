c85282372677c4bef4fa8b9198b086a829265952
Shin dong myung <realdm99@gmail.com>
2020-01-20 16:07:04 +0900

    contents listing

--- chapter6/src/main/java/com/example/realdm99/chapter6/controller/WebBoardController.java
+++ chapter6/src/main/java/com/example/realdm99/chapter6/controller/WebBoardController.java
@@ -1,8 +1,20 @@
 package com.example.realdm99.chapter6.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QSort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.realdm99.chapter6.domain.WebBoard;
+import com.example.realdm99.chapter6.persistence.WebBoardRepository;
+import com.example.realdm99.chapter6.vo.PageVO;
 
 import lombok.extern.java.Log;
 
@@ -11,8 +23,26 @@ import lombok.extern.java.Log;
 @Log
 public class WebBoardController {
 
+//    @GetMapping("/list")
+//    public void list(@PageableDefault(direction = Direction.DESC,
+//    sort = "bno",
+//    size = 10,
+//    page = 0) Pageable page) {
+//        log.info("list() called..." + page);
+//    }
+
+    @Autowired
+    private WebBoardRepository webBoardRepository;
+
     @GetMapping("/list")
-    public void list() {
-        log.info("list() called...");
+    public void list(PageVO vo, Model model) {
+        Pageable page = vo.makePageable(0, "bno");
+
+        Page<WebBoard> result = webBoardRepository.findAll(webBoardRepository.makePredicate(null, null), page);
+
+        log.info("" + page);
+        log.info("" + result);
+
+        model.addAttribute("result", result);
     }
 }
--- chapter6/src/test/java/com/example/realdm99/chapter6/WebBoardRepositoryTests.java
+++ chapter6/src/test/java/com/example/realdm99/chapter6/WebBoardRepositoryTests.java
@@ -5,6 +5,9 @@ import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.annotation.Commit;
 
 import com.example.realdm99.chapter6.domain.WebBoard;
@@ -27,5 +30,31 @@ public class WebBoardRepositoryTests {
 
             webBoardRepository.save(board);
         });
+    }
+
+    @Test
+    public void testList1() {
+        PageRequest pageable = PageRequest.of(0, 20, Direction.DESC, "bno");
+
+        Page<WebBoard> result = webBoardRepository.findAll(webBoardRepository.makePredicate(null, null),
+                                                           pageable);
+
+        System.out.println("PAGE: " + result.getPageable());
+        System.out.println("-------------------");
+
+        result.getContent().forEach(board -> System.out.println("" + board));
+    }
+
+    @Test
+    public void testList2() {
+        PageRequest pageRequest = PageRequest.of(0, 20, Direction.DESC, "bno");
+
+        Page<WebBoard> result = webBoardRepository.findAll(
+                webBoardRepository.makePredicate("t", "10"), pageRequest);
+
+        System.out.println("PAGE: " + result.getPageable());
+
+        System.out.println("------------------------");
+        result.getContent().forEach(board -> System.out.println("" + board));
     }
 }
